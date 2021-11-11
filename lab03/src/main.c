e31a009fc28ee5039546ace94a3dc372e6f4940b
sergeylls <sergeyl1s2003@gmail.com>
2021-11-11 03:26:18 +0200

    lab03 add

---
+++ lab03/src/main.c
@@ -0,0 +1,14 @@
+int main()
+
+{
+
+int znach = 1467;
+   int a = znach / 1000;
+   int b = znach / 100 % 10;
+   int c = znach / 10 % 10;
+   int d = znach % 10;
+
+   int sum = a + b + c + d;
+
+return 0;
+}
