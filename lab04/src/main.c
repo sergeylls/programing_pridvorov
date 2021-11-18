d6f7695530e2e03fe7175d22edfe22ee551cdb3d
sergeylls <sergeyl1s2003@gmail.com>
2021-11-18 02:08:28 +0200

    lab04 add

---
+++ lab04/src/main.c
@@ -0,0 +1,24 @@
+int main()
+{
+ int k = 2;
+ double r = 6;
+ double S = 0;
+ double V = 0;
+ double pi = 3.14;
+ int y = -1;
+ switch (k) {
+ case 1:
+   S = pi * r * r;
+   break;
+ case 2:
+   V = (4 * pi * r * r * r)/3;
+   break;
+ default:
+  y = -1;
+  break;
+
+  return 0;
+}
+
+}
+
